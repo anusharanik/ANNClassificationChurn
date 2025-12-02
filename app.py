a436d7b900c37630a22cde6a21c2aadad9b64f80
anusharanik <43984894+anusharanik@users.noreply.github.com>
2025-12-03 09:34:31 +1100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
+import pandas as pd
+import pickle
+
+#Load the trained model
+model=tf.keras.models.load_model('model.h5')
+
+#Load the encoders and scaler
+
+with open('labelEncoderGender.pkl','rb') as file:
+    labelEncoderGender=pickle.load(file)
+with open('OneHotEncoderGeo.pkl','rb')as file:
+    onehotEncoderGeo=pickle.load(file)
+with open('scaler.pkl','rb') as file:
+    scaler=pickle.load(file)
+
+##Streamlit app
+st.title('Customer Churn Prediction')
+
+#User input
+geography = st.selectbox('Geography', onehotEncoderGeo.categories_[0])
+gender = st.selectbox('Gender', labelEncoderGender.classes_)
+age = st.slider('Age', 18, 92)
+balance = st.number_input('Balance')
+credit_score = st.number_input('Credit Score')
+estimated_salary = st.number_input('Estimated Salary')
+tenure = st.slider('Tenure', 0, 10)
+num_of_products = st.slider('Number of Products', 1, 4)
+has_cr_card = st.selectbox('Has Credit Card', [0, 1])
+is_active_member = st.selectbox('Is Active Member', [0, 1])
+
+# Prepare the input data
+input_data = pd.DataFrame({
+    'CreditScore': [credit_score],
+    'Gender': [labelEncoderGender.transform([gender])[0]],
+    'Age': [age],
+    'Tenure': [tenure],
+    'Balance': [balance],
+    'NumOfProducts': [num_of_products],
+    'HasCrCard': [has_cr_card],
+    'IsActiveMember': [is_active_member],
+    'EstimatedSalary': [estimated_salary]
+})
+
+# One-hot encode 'Geography'
+geo_encoded = onehotEncoderGeo.transform([[geography]]).toarray()
+geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehotEncoderGeo.get_feature_names_out(['Geography']))
+
+# Combine one-hot encoded columns with input data
+input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis=1)
+
+# Scale the input data
+input_data_scaled = scaler.transform(input_data)
+
+
+# Predict churn
+prediction = model.predict(input_data_scaled)
+prediction_proba = prediction[0][0]
+
+st.write(f'Churn Probability: {prediction_proba:.2f}')
+
+if prediction_proba > 0.5:
+    st.write('The customer is likely to churn.')
+else:
+    st.write('The customer is not likely to churn.')
